1a40199d810f4457a1129573e8e368d6a3eefe0c
Izquiano <izquiano@hotmail.com>
2021-01-03 06:23:23 +0100

    Loading

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBP5o72TIq5tlGqSNFDGFEiXz2OQC6tdzw",
+  authDomain: "intagram-dev-880a0.firebaseapp.com",
+  projectId: "intagram-dev-880a0",
+  storageBucket: "intagram-dev-880a0.appspot.com",
+  messagingSenderId: "36505042593",
+  appId: "1:36505042593:web:f0d52f6f150f0665d87ae0",
+  measurementId: "G-B7RMZ7RP3H"
+};  
+
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+
+const Stack = createStackNavigator()
+
+
+export class App extends Component {
+  constructor( props ) {
+    super(props)
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state
+    if(!loaded){
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Tex>
+            Loading
+          </Tex>
+        </View>
+      )
+    }
+    return (
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen 
+          name="Landing"
+          component={ LandingScreen }
+          options={{ headerShown: false }}
+        />  
+        <Stack.Screen 
+          name="Register"
+          component={ RegisterScreen }
+        />  
+      </Stack.Navigator>      
+    </NavigationContainer>
+    )
+  }
+}
+
+
+
